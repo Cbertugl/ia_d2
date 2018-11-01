d5403984ff1396201e03ca9eb312762149a6b18f
Thomas Seignette <tseignette@enseirb-matmeca.fr>
2018-10-31 20:59:42 -0400

    Main un peu plus verbeux, test de la consistance de la variable fait, check d'une contrainte corrigé

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import sudoku
 # ==================================================================================================
 # MAIN
 # ==================================================================================================
+print("Sudoku is:")
 S = sudoku.Sudoku.getSudokuFromFile("sudoku/1.txt")
 S.display()
 
+print("Trying to solve it...")
 CSP = csp.CSP(S)
-
 solvedSudoku = CSP.backtrackingSearch()
 
-if(solvedSudoku != constants.FAILURE): solvedSudoku.display()
+if(solvedSudoku != constants.FAILURE):
+    print("Solved sudoku is:")
+    solvedSudoku.display()
 else: print("Failed to solve the sudoku... :-(")
